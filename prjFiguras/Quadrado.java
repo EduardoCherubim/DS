3fb992a65e7370f7f1cfb2f0f345a9a5774e88d4
EduardoCherubim <142914224+EduardoCherubim@users.noreply.github.com>
2024-08-20 20:39:57 -0300

    Delete prjFiguras directory

--- prjFiguras/Quadrado.java
+++
@@ -1,9 +0,0 @@
-package prjFiguras;
-
-public class Quadrado extends FiguraGeometrica {
-	
-	public void calcularArea() {
-		setResposta(getBase()*getAltura());
-		System.out.println("A area dele e "+getResposta());
-	}
-}
